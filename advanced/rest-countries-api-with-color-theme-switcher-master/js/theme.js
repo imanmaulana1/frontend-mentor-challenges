4585057f36581ad612dc0e59072b057a880e0c3b
Iman Maulana <maulanaiman902@gmail.com>
2024-08-28 17:48:12 +0700

    refactor: simplify darkmode toggle logic

--- advanced/rest-countries-api-with-color-theme-switcher-master/js/theme.js
+++ advanced/rest-countries-api-with-color-theme-switcher-master/js/theme.js
@@ -1,38 +1,24 @@
 $(document).ready(function () {
-  const root = document.documentElement;
-  const theme = localStorage.getItem('darkMode');
+  const body = $('body');
+  const iconDarkMode = $('#icon-theme');
 
-  function setDarkMode() {
-    $('#icon-theme').removeClass('fa-regular').addClass('fa-solid');
-    root.style.setProperty('--background-main', 'hsl(207, 26%, 17%)');
-    root.style.setProperty('--element', 'hsl(209, 23%, 22%)');
-    root.style.setProperty('--text', 'hsl(0, 0%, 100%)');
-  }
-
-  function setLightMode() {
-    $('#icon-theme').removeClass('fa-solid').addClass('fa-regular');
-    root.style.setProperty('--background-main', 'hsl(0, 0%, 98%)');
-    root.style.setProperty('--element', 'hsl(0, 0%, 100%)');
-    root.style.setProperty('--text', 'hsl(200, 15%, 8%)');
-  }
-
-  if (theme === 'active') {
-    $('body').addClass('darkmode');
-    setDarkMode();
-  } else {
-    $('body').removeClass('darkmode');
-    setLightMode();
-  }
-
-  $('.nav-darkmode').on('click', () => {
-    $('body').toggleClass('darkmode');
-
-    if ($('body').hasClass('darkmode')) {
+  function applyTheme(isDarkMode) {
+    if (isDarkMode) {
       localStorage.setItem('darkMode', 'active');
-      setDarkMode();
+      body.addClass('darkmode');
+      iconDarkMode.removeClass('fa-regular').addClass('fa-solid');
     } else {
       localStorage.removeItem('darkMode');
-      setLightMode();
+      body.removeClass('darkmode');
+      iconDarkMode.removeClass('fa-solid').addClass('fa-regular');
     }
+  }
+
+  const theme = localStorage.getItem('darkMode');
+  applyTheme(theme === 'active');
+
+  $('.nav-darkmode').on('click', () => {
+    const isDarkMode = !body.hasClass('darkmode');
+    applyTheme(isDarkMode);
   });
 });
